8b1d20f000bfdcdbd35dc441fc6ef526ff8ed655
Upshur06 <mupshur2005@gmail.com>
2020-05-21 02:13:19 -0400

    rough start

--- food.js
+++ food.js
@@ -1,8 +1,30 @@
-var apiData = {
+
+let apiData = {
   url: 'https://www.themealdb.com/api/json/v1/1/search.php?s=',
-  type: 'food_Category'
+  strMeal: "beef",
 }
 
-var apiUrl = `${apiData.url}${apiData.type}`
+let {url, strMeal} = apiData;
 
-console.log(apiUrl);
+let apiUrl = `${url}${strMeal}`
+
+// console.log(apiUrl);
+
+fetch(apiUrl)
+  .then( (data) => data.json() )
+  // .then( (food) => console.log(food.meals[0].strMeal) )
+  .then( (food) => generateDish(food) )
+
+
+  let generateDish = (data)=>{
+      console.log(data);
+      let html = `
+          <div class="name">Food:  ${data.meals[0].strMeal}</div>
+          <img src=${data.meals[0].strMealThumb} style="width:160px;height:150px;">
+          <div class="type">
+            <span>type: ${data.meals[0].strTags}</span>
+          </div>
+      `
+      let foodDiv = document.querySelector('.food');
+      foodDiv.innerHTML = html;
+  }
